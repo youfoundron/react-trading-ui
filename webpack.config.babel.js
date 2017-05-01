8023b582894405b61f76f4401f781a2fbd9aa73f
Ron Gierlach <rongierlach@gmail.com>
2017-05-01 16:36:56 -0500

    build: init webpack config

---
+++ webpack.config.babel.js
@@ -0,0 +1,35 @@
+import path from 'path'
+import { readdir } from 'fs'
+import makeRule from 'webpack-make-rule'
+
+const srcDir = path.resolve(__dirname, 'src')
+const libDir = path.resolve(__dirname, 'lib')
+
+const entryReducer = fileNames =>
+  fileNames.reduce((entries, fileName) => {
+    const {base, name, ext} = path.parse(fileName)
+    if (ext) entries[name] = path.join('./', base)
+    return entries
+  }, {})
+
+const dynamicEntry = () => new Promise(
+  (resolve, reject) =>
+    readdir(srcDir, (err, files) => err
+      ? reject(err)
+      : resolve(entryReducer(files))
+    )
+  )
+
+export default {
+  context: srcDir,
+  entry: dynamicEntry,
+  output: {
+    path: libDir,
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      makeRule(/\.jsx?$/, 'babel-loader')
+    ]
+  }
+}
