1f726b364f03933426ad714c1adf7650223248f9
Ron Gierlach <rongierlach@gmail.com>
2017-05-01 14:26:41 -0500

    test: add test entry and a single remedial test

---
+++ test/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    Hello World
+  </div>
+)
+
+export default App
---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+/* global it, expect, describe */
+import React from 'react'
+import { mount } from 'enzyme'
+import App from './App'
+
+describe('Test Suite', () => {
+  it('Has at least one test', () => {
+    const wrapper = mount(<App />)
+
+    expect(wrapper.text()).toEqual('Hello World')
+  })
+})
