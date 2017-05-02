133b7d8664cfc5354942526bafc5913407500e92
Ron Gierlach <rongierlach@gmail.com>
2017-05-01 23:16:27 -0500

    feat: start adding logic to OrderBook component

--- src/OrderBook.js
+++ src/OrderBook.js
@@ -1,24 +1,68 @@
+import R from 'ramda'
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// Defaults
+import * as getters from './defaults/getters'
+// import * as renderers from './defaults/renderers'
+
+// Components
+import FintechUIParent from './components/FintechUIParent'
+import FintechUIHeader from './components/FintechUIHeader'
+import FintechUIContent from './components/FintechUIContent'
+
+const unsafePropNames = [
+  'asks',
+  'bids',
+  'depth',
+  'headerText',
+  'getSize',
+  'getPrice',
+  'getPosition'
+]
+
 class OrderBook extends React.Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = { hasScrolled: false }
+    this.centerSpread = this.centerSpread.bind(this)
+  }
+
+  centerSpread () {
+    console.log('scroll so that spread is centered')
+  }
+
   render () {
+    const { headerText } = this.props
     return (
-      <div>
-        order book
-      </div>
+      <FintechUIParent {...R.omit(unsafePropNames, this.props)}>
+        <FintechUIHeader>{headerText}</FintechUIHeader>
+        <FintechUIContent>
+          {/* <Spread onClick={centerSpread} /> */}
+        </FintechUIContent>
+      </FintechUIParent>
     )
   }
 }
 
 OrderBook.propTypes = {
   asks: PropTypes.array,
-  bids: PropTypes.array
+  bids: PropTypes.array,
+  depth: PropTypes.number,
+  headerText: PropTypes.string,
+  getSize: PropTypes.func,
+  getPrice: PropTypes.func,
+  getPosition: PropTypes.func
 }
 
 OrderBook.defaultProps = {
   asks: [],
-  bids: []
+  bids: [],
+  depth: 50,
+  headerText: 'Order Book',
+  getSize: getters.getSize,
+  getPrice: getters.getPrice,
+  getPosition: getters.getPosition
 }
 
 export default OrderBook
