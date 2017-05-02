aaa6e9d1aa8b0923f775e563441966512cadace7
Ron Gierlach <rongierlach@gmail.com>
2017-05-01 21:39:24 -0500

    feat: add color defaults for components to reference

---
+++ src/defaults/colors.js
@@ -0,0 +1,14 @@
+export default {
+  buyPrimary: '#4DA53C',        // green
+  buySecondary: '#437944',      // muted green
+
+  sellPrimary: '#FF6939',       // orange
+  sellSecondary: '#7F4332',     // muted orange
+
+  textHighContrast: '#CED2D5',  // near white
+  textMedContrast: '#7E878C',   // light grey
+  textLowContrast: '#3B464E',   // normal grey
+
+  headerBackground: '#2F3D45',  // med-light grey
+  parentBackground: '#1E2B34'   // darkest grey
+}
