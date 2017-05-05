9f09784f7c925196eb34574d99b6574c7d9c44c7
Ron Gierlach <rongierlach@gmail.com>
2017-05-04 19:37:53 -0500

    put it all together, order book

--- src/OrderBook.js
+++ src/OrderBook.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types'
 // Defaults
 import * as util from './defaults/util'
 import * as getters from './defaults/getters'
-// import * as renderers from './defaults/renderers'
 
 // Components
 import FintechUIParent from './components/FintechUIParent'
@@ -13,20 +12,23 @@ import FintechUIHeader from './components/FintechUIHeader'
 import FintechUIContentWrapper from './components/FintechUIContentWrapper'
 import FintechUIStickyContent from './components/FintechUIStickyContent'
 import FintechUITableHead from './components/FintechUITableHead'
+import FintechUITableHeading from './components/FintechUITableHeading'
 import FintechUIScrollingContent from './components/FintechUIScrollingContent'
+import FintechUIOrderTable from './components/FintechUIOrderTable'
+import FintechUIOrder from './components/FintechUIOrder'
+import PrettySize from './components/PrettySize'
+import PrettyPrice from './components/PrettyPrice'
+import PrettyPosition from './components/PrettyPosition'
+import Spread from './components/Spread'
 import Spinner from './components/Spinner'
 
 const unsafePropNames = [
-  'asks',
-  'bids',
-  'depth',
-  'headerText',
-  'sizeLabel',
-  'priceLabel',
-  'positionLabel',
-  'getSize',
-  'getPrice',
-  'getPosition'
+  'asks', 'bids', 'depth', 'headerText', 'spreadText', 'showSizeBar',
+  'sizeLabel', 'priceLabel', 'positionLabel', 'onClickOrder',
+  'sizeBarMaxWidth', 'sizeBarMaxSize', 'sizeBarUnitSize',
+  'getSize', 'getPrice', 'getPosition',
+  'sizeFormat', 'priceFormat', 'positionFormat', 'spreadFormat',
+  'renderSize', 'renderPrice', 'renderPosition'
 ]
 
 class OrderBook extends React.Component {
@@ -41,22 +43,83 @@ class OrderBook extends React.Component {
   }
 
   render () {
-    const { headerText, sizeLabel, priceLabel, positionLabel } = this.props
+    const {
+      asks, bids, depth, headerText, spreadText, showSizeBar,
+      sizeLabel, priceLabel, positionLabel, onClickOrder,
+      sizeBarMaxWidth, sizeBarMaxSize, sizeBarUnitSize,
+      getSize, getPrice, getPosition,
+      sizeFormat, priceFormat, positionFormat, spreadFormat,
+      renderSize, renderPrice, renderPosition
+    } = this.props
+    const safeProps = R.omit(unsafePropNames, this.props)
+    const hasOrders = util.hasReceivedOrderBook(this.props)
+    const visibleAsks = asks.reverse().slice(0, depth)
+    const visibleBids = bids.slice(0, depth)
+    const spread = hasOrders ? getPrice(R.last(visibleAsks)) - getPrice(R.head(visibleBids)) : undefined
+    const dataConfigs = [
+      {propName: 'size', format: sizeFormat, getter: getSize, renderer: renderSize},
+      {propName: 'price', format: priceFormat, getter: getPrice, renderer: renderPrice},
+      {propName: 'position', format: positionFormat, getter: getPosition, renderer: renderPosition}
+    ]
     return (
-      <FintechUIParent {...R.omit(unsafePropNames, this.props)}>
+      <FintechUIParent {...safeProps}>
+        {/* UI HEADER */}
         <FintechUIHeader>{headerText}</FintechUIHeader>
         <FintechUIContentWrapper>
           <FintechUIStickyContent>
+            {/* TABLE COLUMN HEADERS */}
             <FintechUITableHead>
-              <th>{sizeLabel}</th>
-              <th>{priceLabel}</th>
-              <th>{positionLabel}</th>
+              {showSizeBar ? <FintechUITableHeading style={{width: sizeBarMaxWidth}} /> : null}
+              <FintechUITableHeading>{sizeLabel}</FintechUITableHeading>
+              <FintechUITableHeading>{priceLabel}</FintechUITableHeading>
+              <FintechUITableHeading>{positionLabel}</FintechUITableHeading>
             </FintechUITableHead>
           </FintechUIStickyContent>
           <FintechUIScrollingContent>
-
-            <Spinner hide={util.hasReceivedOrderBook(this.props)} />
-
+            {/* ASKS TABLE */}
+            <FintechUIOrderTable
+              style={{marginTop: '4em'}}
+              {...{showSizeBar, sizeLabel, priceLabel, positionLabel}}
+            >
+              {visibleAsks.map((order, i) =>
+                <FintechUIOrder
+                  key={i}
+                  side='sell'
+                  order={order}
+                  size={getSize(order)}
+                  onClick={onClickOrder}
+                  dataConfigs={dataConfigs}
+                  showSizeBar={showSizeBar}
+                  sizeBarMaxSize={sizeBarMaxSize}
+                  sizeBarUnitSize={sizeBarUnitSize}
+                  sizeBarMaxWidth={sizeBarMaxWidth}
+                />
+              )}
+            </FintechUIOrderTable>
+            {/* SPREAD MARKER */}
+            <Spread spread={spread} label={spreadText} format={spreadFormat} hide={!hasOrders} />
+            {/* BIDS TABLE */}
+            <FintechUIOrderTable
+              style={{marginBottom: '6em'}}
+              {...{showSizeBar, sizeLabel, priceLabel, positionLabel}}
+            >
+              {visibleBids.map((order, i) =>
+                <FintechUIOrder
+                  key={i}
+                  side='buy'
+                  order={order}
+                  size={getSize(order)}
+                  onClick={onClickOrder}
+                  dataConfigs={dataConfigs}
+                  showSizeBar={showSizeBar}
+                  sizeBarMaxSize={sizeBarMaxSize}
+                  sizeBarUnitSize={sizeBarUnitSize}
+                  sizeBarMaxWidth={sizeBarMaxWidth}
+                />
+              )}
+            </FintechUIOrderTable>
+            {/* LOADING SPINNER */}
+            <Spinner hide={hasOrders} />
           </FintechUIScrollingContent>
         </FintechUIContentWrapper>
       </FintechUIParent>
@@ -68,26 +131,51 @@ OrderBook.propTypes = {
   asks: PropTypes.array,
   bids: PropTypes.array,
   depth: PropTypes.number,
+  sizeBarMaxWidth: PropTypes.number,
+  sizeBarMaxSize: PropTypes.number,
+  sizeBarUnitSize: PropTypes.number,
+  showSizeBar: PropTypes.bool,
   headerText: PropTypes.string,
+  spreadText: PropTypes.string,
   sizeLabel: PropTypes.string,
   priceLabel: PropTypes.string,
   positionLabel: PropTypes.string,
   getSize: PropTypes.func,
   getPrice: PropTypes.func,
-  getPosition: PropTypes.func
+  getPosition: PropTypes.func,
+  sizeFormat: PropTypes.string,
+  priceFormat: PropTypes.string,
+  positionFormat: PropTypes.string,
+  spreadFormat: PropTypes.string,
+  renderSize: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+  renderPrice: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+  renderPosition: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+  onClickOrder: PropTypes.func
 }
 
 OrderBook.defaultProps = {
   asks: [],
   bids: [],
-  depth: 50,
+  depth: Infinity,
+  showSizeBar: true,
+  sizeBarMaxWidth: 50,
+  sizeBarMaxSize: 1000,
+  sizeBarUnitSize: 50,
   headerText: 'Order Book',
+  spreadText: 'USD SPREAD',
   sizeLabel: 'Market Size',
   priceLabel: 'Price (USD)',
-  positionLabel: 'My Size',
+  positionLabel: 'My Position',
   getSize: getters.getSize,
   getPrice: getters.getPrice,
-  getPosition: getters.getPosition
+  getPosition: getters.getPosition,
+  sizeFormat: '0.00000000',
+  priceFormat: '00.00',
+  positionFormat: '0.00000000',
+  spreadFormat: '0.00',
+  renderSize: PrettySize,
+  renderPrice: PrettyPrice,
+  renderPosition: PrettyPosition
 }
 
 export default OrderBook
