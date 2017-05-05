030adac49c0256d09221e77b4c7aad5838d65b47
Ron Gierlach <rongierlach@gmail.com>
2017-05-04 19:15:21 -0500

    utils

---
+++ src/defaults/util.js
@@ -0,0 +1,14 @@
+export const hasReceivedOrderBook = ({ bids, asks }) =>
+  Array.isArray(bids) &&
+  Array.isArray(asks) &&
+  bids.length > 0 &&
+  asks.length > 0
+
+export const countTrailingZeroes = numString => {
+  let numZeroes = 0
+  for (let digit of numString.split('').reverse()) {
+    if (digit == 0) ++numZeroes // eslint-disable-line eqeqeq
+    else return numZeroes
+  }
+  return numZeroes
+}
