8fd876b337ca26f4a627c2a511bc19bb0c447b2f
Ron Gierlach <rongierlach@gmail.com>
2017-05-04 19:19:37 -0500

    spread component

---
+++ src/components/Spread.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import glamorous from 'glamorous'
+
+import colors from '../defaults/colors'
+import PrettyPrice from './PrettyPrice'
+
+const SpreadDiv = glamorous.div({
+  textAlign: 'center',
+  padding: '.3em 0',
+  color: colors.textMedContrast,
+  borderWidth: '.5px 0',
+  borderColor: colors.textMedContrast,
+  borderStyle: 'solid',
+  fontWeight: 700,
+  fontSize: '.8em',
+  '.hide': {
+    display: 'none'
+  },
+  ':hover': {
+    cursor: 'pointer'
+  }
+})
+
+const Spread = ({spread, label, format, ...props}) => (
+  <SpreadDiv {...props}>
+    <PrettyPrice price={spread} format={format} />
+    &nbsp; - &nbsp;
+    {label}
+  </SpreadDiv>
+)
+
+Spread.propTypes = {
+  spread: PropTypes.number,
+  format: PropTypes.string,
+  spreadText: PropTypes.string
+}
+
+Spread.defaultProps = {
+  spread: 0,
+  format: '0.00',
+  spreadText: 'SPREAD'
+}
+
+export default Spread
