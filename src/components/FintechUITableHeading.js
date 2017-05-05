d6cd0ba8daf0e403b4ec15dcb4538ce463ebf3b0
Ron Gierlach <rongierlach@gmail.com>
2017-05-04 19:18:47 -0500

    table heading

---
+++ src/components/FintechUITableHeading.js
@@ -0,0 +1,9 @@
+import glamorous from 'glamorous'
+
+const FintechUITableHeading = glamorous.th({
+  fontWeight: 400,
+  textAlign: 'right',
+  transform: 'translateX(-10%)'
+})
+
+export default FintechUITableHeading
