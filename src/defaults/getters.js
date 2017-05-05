48ca1c42828f5c36e2ff4a92d354d524c3c1d5cc
Ron Gierlach <rongierlach@gmail.com>
2017-05-05 17:34:57 -0500

    trade history getters

--- src/defaults/getters.js
+++ src/defaults/getters.js
@@ -1,3 +1,5 @@
 export const getSize = data => data.size
 export const getPrice = data => data.price
 export const getPosition = data => data.position
+export const getTimeStamp = data => data.time
+export const getSide = data => data.side
