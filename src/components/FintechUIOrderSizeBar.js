60f03e1c18bba15247d1a14ae2dc63e51289897b
Ron Gierlach <rongierlach@gmail.com>
2017-05-04 19:17:55 -0500

    size bar

---
+++ src/components/FintechUIOrderSizeBar.js
@@ -0,0 +1,20 @@
+import glamorous from 'glamorous'
+import colors from '../defaults/colors'
+
+const FintechUIOrderSizeBar = glamorous.td({
+  padding: 0,
+  boxSizing: 'border-box'
+}, ({showSizeBar, side, size, sizeBarMaxWidth, sizeBarMaxSize, sizeBarUnitSize}) => {
+  const totalUnits = sizeBarMaxSize / sizeBarUnitSize
+  const numUnits = Math.floor(size / sizeBarUnitSize)
+  const percentSize = numUnits >= totalUnits ? 1 : numUnits / totalUnits
+  const sizeBarWidth = percentSize * sizeBarMaxWidth
+  return {
+    width: sizeBarMaxWidth,
+    borderColor: side === 'buy' ? colors.buySecondary : colors.sellSecondary,
+    borderStyle: showSizeBar ? 'solid' : 'none',
+    borderWidth: `0 0 0 ${1 + sizeBarWidth}px`
+  }
+})
+
+export default FintechUIOrderSizeBar
