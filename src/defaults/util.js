b420aa1193dc1797cf05e685e0e484b468a034b4
Ron Gierlach <rongierlach@gmail.com>
2017-05-05 19:17:22 -0500

    remove log

--- src/defaults/util.js
+++ src/defaults/util.js
@@ -4,10 +4,8 @@ export const hasReceivedOrderBook = ({ bids, asks }) =>
   bids.length > 0 &&
   asks.length > 0
 
-export const hasReceivedTrades = ({ trades }) => {
-  console.log({trades})
-  return Array.isArray(trades) && trades.length > 0
-}
+export const hasReceivedTrades = ({ trades }) =>
+  Array.isArray(trades) && trades.length > 0
 
 export const countTrailingZeroes = numString => {
   let numZeroes = 0
