1203e8959354e027274ea530bd6a402ce75b289e
Ron Gierlach <rongierlach@gmail.com>
2017-05-08 16:52:12 -0500

    default productId -> ETH-USD

--- demo/app/constants/actionCreators.js
+++ demo/app/constants/actionCreators.js
@@ -1,11 +1,11 @@
 import types from './actionTypes'
 
-export const fetchBook = (productId = 'BTC-USD', level = 2) => ({
+export const fetchBook = (productId = 'ETH-USD', level = 2) => ({
   type: types.BOOK_REQUEST,
   payload: { productId, level }
 })
 
-export const fetchTrades = (productId = 'BTC-USD') => ({
+export const fetchTrades = (productId = 'ETH-USD') => ({
   type: types.TRADES_REQUEST,
   payload: { productId }
 })
