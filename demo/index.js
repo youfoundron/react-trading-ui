479a6761698c3f4a284ee5658fffc5c319466047
Ron Gierlach <rongierlach@gmail.com>
2017-05-08 16:04:46 -0500

    mount demo app in dev server script

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,6 @@
+import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
