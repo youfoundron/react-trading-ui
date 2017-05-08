d3d41963dc988022b318c4b1a8e65a91e7ca9ae4
Ron Gierlach <rongierlach@gmail.com>
2017-05-08 16:03:25 -0500

    create RealTimeTradeHistory container for demo app

---
+++ demo/app/containers/RealTimeTradeHistory.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import collect from '../utils/collect'
+import TradeHistory from '../../../src/TradeHistory'
+import {selectTrades} from '../constants/selectors'
+
+class RealTimeTradeHistory extends React.Component {
+  componentWillMount () {
+    this.props.fetchTrades()
+  }
+
+  render () {
+    return (
+      <TradeHistory
+        trades={this.props.trades}
+        style={{
+          right: 0,
+          maxWidth: '50%',
+          position: 'absolute'
+        }}
+      />
+    )
+  }
+}
+
+export default collect(
+  [selectTrades, 'trades'],
+  [['actions', 'fetchTrades']]
+)(RealTimeTradeHistory)
