dae0d83f226a78cc00aea14246a7106fd9109cdb
Ron Gierlach <rongierlach@gmail.com>
2017-05-08 17:00:13 -0500

    remove OpenOrders

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import OrderBook from './OrderBook'
-import OpenOrders from './OpenOrders'
 import DepthChart from './DepthChart'
 import PriceChart from './PriceChart'
 import TradeHistory from './TradeHistory'
 
 export {
   OrderBook,
-  OpenOrders,
   DepthChart,
   PriceChart,
   TradeHistory
