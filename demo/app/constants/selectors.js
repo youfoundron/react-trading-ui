4d93e8639a02ad46c3b1a7ea8c9909711428e5f5
Ron Gierlach <rongierlach@gmail.com>
2017-05-08 15:57:00 -0500

    add demo app selectors constants

---
+++ demo/app/constants/selectors.js
@@ -0,0 +1,4 @@
+export const selectAsks = state => state.get('asks')
+export const selectBids = state => state.get('bids')
+export const selectTrades = state => state.get('trades')
+export const selectError = state => state.get('error')
