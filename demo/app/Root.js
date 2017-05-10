170dc831205da82018ec6c28362da261a2cf5a80
Ron Gierlach <rongierlach@gmail.com>
2017-05-09 21:23:09 -0500

    separate Provider and app root in order to add lifecyle method

---
+++ demo/app/Root.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import collect from './utils/collect'
+import RealTimeOrderBook from './containers/RealTimeOrderBook'
+import RealTimeTradeHistory from './containers/RealTimeTradeHistory'
+
+class Root extends React.Component {
+  componentWillMount () {
+    this.props.connectFeed()
+  }
+
+  render () {
+    return (
+      <div className='root'>
+        <RealTimeOrderBook />
+        <RealTimeTradeHistory />
+        <style jsx global>{`
+          html { line-height: 1.15em; }
+          html, body, #root, .demo { height: 100%; }
+          body { margin: 0; font-family: 'Open Sans', monospace; }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+export default collect(
+  [['actions', 'connectFeed']]
+)(Root)
--- demo/app/index.js
+++ demo/app/index.js
@@ -2,20 +2,11 @@ import React from 'react'
 import {Provider} from 'react-redux'
 
 import store from './store'
-import RealTimeOrderBook from './containers/RealTimeOrderBook'
-import RealTimeTradeHistory from './containers/RealTimeTradeHistory'
+import Root from './Root'
 
 const App = () => (
   <Provider store={store}>
-    <div className='app'>
-      <RealTimeOrderBook />
-      <RealTimeTradeHistory />
-      <style jsx global>{`
-        html { line-height: 1.15em; }
-        html, body, #root, .demo { height: 100%; }
-        body { margin: 0; font-family: 'Open Sans', monospace; }
-      `}</style>
-    </div>
+    <Root />
   </Provider>
 )
 
