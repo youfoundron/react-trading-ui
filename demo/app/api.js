14504f0d77e4ed0e8b07d9d1351c357d57b41510
Ron Gierlach <rongierlach@gmail.com>
2017-05-09 21:23:31 -0500

    add feed creation to api

--- demo/app/api.js
+++ demo/app/api.js
@@ -1,6 +1,7 @@
 import serialize from './utils/serialize'
 
 const baseUrl = 'https://api.gdax.com/products'
+const feedUrl = 'wss://ws-feed.gdax.com'
 
 export const fetchBook = ({productId, ...params}) =>
   window.fetch(`${baseUrl}/${productId}/book?${serialize(params)}`)
@@ -9,3 +10,6 @@ export const fetchBook = ({productId, ...params}) =>
 export const fetchTrades = ({productId, ...params}) =>
   window.fetch(`${baseUrl}/${productId}/trades?${serialize(params)}`)
     .then(resp => resp.json())
+
+export const createFeed = () =>
+  new window.WebSocket(feedUrl)
