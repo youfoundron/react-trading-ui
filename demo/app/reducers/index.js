4d5f00faf7b9f53021eee52f82d8280b3984d176
Ron Gierlach <rongierlach@gmail.com>
2017-05-09 22:22:56 -0500

    combine reducers

--- demo/app/reducers/index.js
+++ demo/app/reducers/index.js
@@ -1,22 +1,11 @@
-import types from '../constants/actionTypes'
-import initialState from '../store/initialState'
+import { combineReducers } from 'redux-immutable'
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case (types.BOOK_SUCCESS):
-      return state
-        .set('asks', action.payload.asks)
-        .set('bids', action.payload.bids)
-    case (types.BOOK_FAILURE):
-      return state.set('error', action.payload)
+import error from './error'
+import book from './book'
+import trades from './trades'
 
-    case (types.TRADES_SUCCESS):
-      return state.set('trades', action.payload)
-    case (types.TRADES_FAILURE):
-      return state.set('error', action.payload)
-    default:
-      return state
-  }
-}
-
-export default rootReducer
+export default combineReducers({
+  error,
+  book,
+  trades
+})
