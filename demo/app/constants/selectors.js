edd721dd8bd904ff16221a697df187a3ffbba6af
Ron Gierlach <rongierlach@gmail.com>
2017-05-09 22:21:47 -0500

    use reselect to create memoized selectors in constants/selectors.js

--- demo/app/constants/selectors.js
+++ demo/app/constants/selectors.js
@@ -1,4 +1,9 @@
-export const selectAsks = state => state.get('asks')
-export const selectBids = state => state.get('bids')
-export const selectTrades = state => state.get('trades')
+import { createSelector } from 'reselect'
+
 export const selectError = state => state.get('error')
+
+export const selectBook = state => state.get('book')
+export const selectAsks = createSelector(selectBook, book => book.get('asks'))
+export const selectBids = createSelector(selectBook, book => book.get('bids'))
+
+export const selectTrades = state => state.get('trades')
