9f94f32920f46614c2a425a58a0b83283f7fbfa8
Ron Gierlach <rongierlach@gmail.com>
2017-05-23 18:58:08 -0500

    add trades on match order messages

--- demo/app/reducers/trades.js
+++ demo/app/reducers/trades.js
@@ -1,16 +1,27 @@
+import { pick } from 'ramda'
+import { List } from 'immutable'
+
 import types from '../constants/actionTypes'
 import initialState from '../store/initialState'
 import { selectTrades } from '../constants/selectors'
 
 const defaultState = selectTrades(initialState)
+const sanitizeMessage = pick([
+  'time',
+  'price',
+  'size',
+  'side'
+])
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case types.TRADES_SUCCESS :
-      return action.payload
+      return List(action.payload)
 
-    // case types.SOCKET_MESSAGE :
-    //   return action.payload
+    case types.SOCKET_MESSAGE :
+      return action.payload.type === 'match'
+        ? state.unshift(sanitizeMessage(action.payload))
+        : state
 
     default:
       return state
