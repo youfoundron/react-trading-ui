c330e1ca95506461d1909712bfbb9746c4fe903e
Ron Gierlach <rongierlach@gmail.com>
2017-05-23 18:57:30 -0500

    connect book reducer to broken orderbook logic

--- demo/app/reducers/book.js
+++ demo/app/reducers/book.js
@@ -1,27 +1,22 @@
 import types from '../constants/actionTypes'
 import initialState from '../store/initialState'
 import { selectBook } from '../constants/selectors'
+import * as messages from '../utils/messages'
 
 const defaultState = selectBook(initialState)
 
-const seedBook = payload => prevBook => payload.reduce(
-  (newBook, [price, size, numOrders]) =>
-    newBook.set(price, {price, size, numOrders})
-    , prevBook
-)
-
+/* Book Balancing Logic:
+ * https://docs.gdax.com/#real-time-order-book
+ */
 export default (state = defaultState, action) => {
   switch (action.type) {
     case types.BOOK_SUCCESS :
-      return state
-        .update('asks', seedBook(action.payload.asks))
-        .update('bids', seedBook(action.payload.bids))
-        .set('snapshot', action.payload.snapshot)
+      return messages.applyQueuedMessages(state, action.payload)
 
-    // case types.SOCKET_MESSAGE :
-    //   return state.get('snapshot')
-    //     ? state
-    //     : state.update('queuedMessages', queue => queue.push(action.payload))
+    case types.SOCKET_MESSAGE :
+      return state.get('hasReceivedSnapshot')
+        ? messages.applyMessage(state, action.payload)
+        : messages.queueMessage(state, action.payload)
 
     default:
       return state
